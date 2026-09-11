8320f1feb3d3e0a4bf27f936213a79a295885605
Maryleksa <mary_baranova@bk.ru>
2026-09-11 22:11:23 +0300

    feat: add invitation model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,9 @@
 from app.models.application import Application
+from app.models.invitation import Invitation
 from app.models.participant import Participant
 
 __all__ = [
     "Application",
+    "Invitation",
     "Participant",
 ]
