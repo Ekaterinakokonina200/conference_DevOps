7776426b598a2a0d9f4e19f2bb3569b9af395b0f
amulanga otkhonova <eamulanga.oth@mail.ru>
2026-09-12 20:43:33 +0000

    feat: add hotel request model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.application import Application
+from app.models.hotel_request import HotelRequest
 from app.models.invitation import Invitation
 from app.models.participant import Participant
 from app.models.payment import Payment
@@ -6,6 +7,7 @@ from app.models.thesis import Thesis
 
 __all__ = [
     "Application",
+    "HotelRequest",
     "Invitation",
     "Participant",
     "Payment",
---
+++ app/models/hotel_request.py
@@ -0,0 +1,35 @@
+﻿from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer
+
+from app.database import Base
+
+
+class HotelRequest(Base):
+    __tablename__ = "hotel_requests"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True,
+    )
+
+    participant_id = Column(
+        Integer,
+        ForeignKey("participants.id"),
+        nullable=False,
+    )
+
+    required = Column(
+        Boolean,
+        nullable=False,
+        default=False,
+    )
+
+    check_in = Column(
+        Date,
+        nullable=True,
+    )
+
+    check_out = Column(
+        Date,
+        nullable=True,
+    )
